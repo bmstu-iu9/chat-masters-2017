fe3425ba9e5f6e58ceb1129ad9848aa0f1ee07ef
nstrel <strelka1302@gmail.com>
2017-11-17 23:35:04 +0300

    added new functions to dbinterface.hs

--- dbinterface.hs
+++ dbinterface.hs
@@ -3,6 +3,8 @@ module DBInterface where
 import Database.HDBC
 import Database.HDBC.Sqlite3
 import Data.List
+import Data.Maybe
+import Data.Time
 
 joinByComma :: [String] -> String
 joinByComma = intercalate ","
@@ -43,9 +45,20 @@ selectX :: IConnection conn => conn -> String -> [String] -> String -> IO Statem
 selectX conn table items criteria = prepare conn $ makeSelect table items criteria
 deleteX :: IConnection conn => conn -> String -> String -> IO Statement
 deleteX conn table criteria = prepare conn $ makeDelete table criteria
+selectAllX :: IConnection conn => conn -> String -> IO [[(String, SqlValue)]]
+selectAllX conn x = do
+  sel_stmt <- selectX conn x [] []
+  _ <- execute sel_stmt []
+  fetchAllRowsAL sel_stmt
 
 insertUsers :: IConnection conn => conn -> IO Statement
 insertUsers conn = prepare conn $ makeInsert "users" ["username", "password"]
+
+insertUser :: IConnection conn => conn -> String -> String -> IO ()
+insertUser conn name pass = do
+    ins_stmt <- insertUsers conn
+    _ <- execute ins_stmt [toSql name, toSql pass]
+    commit conn
 
 updateUsers :: IConnection conn => conn -> [String] -> IO Statement
 updateUsers conn items = prepare conn $ makeUpdate "users" items (relate "u_id")
@@ -53,48 +66,66 @@ updateUsers conn items = prepare conn $ makeUpdate "users" items (relate "u_id")
 deleteUsers :: IConnection conn => conn -> IO Statement
 deleteUsers conn = deleteX conn "users" (relate "u_id")
 
-selectAllUsers :: IConnection conn => conn -> IO [[SqlValue]]
-selectAllUsers conn = quickQuery' conn "SELECT * FROM users" []
-
-selectAllUsers_stmt :: IConnection conn => conn -> IO Statement
-selectAllUsers_stmt conn = selectX conn "users" [] []
+selectAllUsers :: IConnection conn => conn -> IO [[(String, SqlValue)]]
+selectAllUsers conn = selectAllX conn "users"
 
 selectUsersById :: IConnection conn => conn -> IO Statement
 selectUsersById conn = selectX conn "users" [] (relate "u_id")
 
-selectPassByName :: IConnection conn => conn -> IO Statement
-selectPassByName conn = selectX conn "users" ["password"] (relate "username")
+selectPassByName :: IConnection conn => conn -> String -> IO String
+selectPassByName conn name = do
+  sel_stmt <- selectX conn "users" ["password"] (relate "username")
+  _ <- execute sel_stmt [toSql (name :: String)]
+  x <- fetchRow sel_stmt
+  return $ if isJust x then fromSql (head (fromJust x)) :: String else ""
 
-selectUsers :: IConnection conn => conn -> Int -> IO [[SqlValue]]
-selectUsers conn uid = quickQuery' conn "SELECT * FROM users WHERE u_id=?" [toSql uid]
+selectUsers :: IConnection conn => conn -> Int -> IO (Maybe [(String, SqlValue)])
+selectUsers conn uid = do
+  sel_stmt <- selectX conn "users" [] (relate "u_id")
+  _ <- execute sel_stmt [toSql uid]
+  fetchRowAL sel_stmt
 
 --
 selectUserByName' :: IConnection conn => conn -> String -> IO [[SqlValue]]
 selectUserByName' conn name = quickQuery' conn "SELECT password FROM users WHERE username=?" [toSql name]
+selectUserByName :: IConnection conn => conn -> String -> IO String
 selectUserByName conn name = do
   x <- selectUserByName' conn name
-  let y = if (null x) then [] else head x
-  let a = if (null y) then "" else (fromSql (head y)) :: [Char]
+  let y = if null x then [] else head x
+  let a = if null y then "" else fromSql (head y) :: String
   return a
 
 insertChat :: IConnection conn => conn -> IO Statement
 insertChat conn = prepare conn $ makeInsert "chat" ["c_uid", "c_body"]
 
+insertMsg :: IConnection conn => conn -> String -> String -> IO ()
+insertMsg conn name msg = do
+    let sel_stmt = makeSelect "users" ["u_id"] (relate "username")
+    uid <- quickQuery' conn sel_stmt [toSql name]
+    ins_stmt <- insertChat conn
+    _ <- execute ins_stmt (head uid ++ [toSql msg])
+    commit conn
+
+selectMsgs :: IConnection conn => conn -> IO [(String, String, UTCTime)]
+selectMsgs conn = do
+    sel <- selectX conn "chat" ["c_username", "c_body", "c_post_time"] []
+    _ <- execute sel []
+    sql_tmp <- fetchAllRows sel
+    return $ map (\(u:b:t:_) ->
+        (fromSql u::String, fromSql b::String, fromSql t::UTCTime)) sql_tmp
+
 updateChat :: IConnection conn => conn -> IO Statement
 updateChat conn = prepare conn $ makeUpdate "chat" ["c_body"] (relate "c_id")
 
 deleteChat :: IConnection conn => conn -> IO Statement
-deleteChat conn = prepare conn "INSERT INTO hidden_message(hm_uid,hm_mid) VALUES (?,?)"
+deleteChat conn = prepare conn $ makeInsert "hidden_messages" ["hm_uid", "hm_mid"]
 
 undoDeleteChat :: IConnection conn => conn -> IO Statement
 undoDeleteChat conn = deleteX conn "hidden_messages" criteria
     where criteria = makeAndWheres ["hm_uid", "hm_mid"]
 
 selectAllChat :: IConnection conn => conn -> IO [[(String, SqlValue)]]
-selectAllChat conn = do
-  sel_stmt <- selectX conn "users" [] []
-  _ <- execute sel_stmt []
-  fetchAllRowsAL sel_stmt
+selectAllChat conn = selectAllX conn "chat"
 
 selectUserChat :: IConnection conn => conn -> Int -> IO [[SqlValue]]
 selectUserChat conn uid = quickQuery' conn "SELECT * FROM chat WHERE c_mid NOT IN \
@@ -135,7 +166,8 @@ insertSampleData conn = do
                        ,[toSql (3 :: Int), toSql (4 :: Int)]
                        ]
 
-getSqlConnection db = connectSqlite3 db
+getSqlConnection :: FilePath -> IO Connection
+getSqlConnection = connectSqlite3
 
 {-
 main = do
