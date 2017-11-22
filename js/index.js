4385c33c87226b7c0131118a2ad473eb12872175
ahanika <avmalich@gmail.com>
2017-11-22 15:07:45 +0300

    JS

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 "use strict";
 
-var email = document.getElementsByClassName('emailField')[0];
-var login = document.getElementsByClassName('loginField')[0];
+var email1 = document.getElementsByClassName('emailField')[0];
+var login1 = document.getElementsByClassName('loginField')[0];
 
 function checkEmail(){
 	validateEmail(function(err)
 	{
 		if(err)
 		{
-			email.style.boxShadow =  "0 0 4px 0 #f00";
+			email1.style.boxShadow =  "0 0 4px 0 #f00";
 			window.alert("E-mail занят!");
 		}
 		else
 		{
-			email.style.boxShadow = "0 0 4px 0 #0f0";
+			email1.style.boxShadow = "0 0 4px 0 #0f0";
 			enableSubmit();
 		}
 	})
@@ -21,7 +21,7 @@ function checkEmail(){
 
  
 function validateEmail(callback) {
-    var emailToCheck = $('#emailField').val();
+    var emailToCheck = email1.value;;
 	$.get('checkingEmail.hs', {email: emailToCheck})
 		.success(function() { callback(false) })
 		.error(function() { callback(true) })
@@ -32,26 +32,28 @@ function checkLogin(){
 	{
 		if(err)
 		{
-			login.style.boxShadow =  "0 0 4px 0 #f00";
+			login1.style.boxShadow =  "0 0 4px 0 #f00";
 			window.alert("Выберите другое имя!");
+
 		}
 		else
 		{
-			login.style.boxShadow = "0 0 4px 0 #0f0";
+			login1.style.boxShadow = "0 0 4px 0 #0f0";
 			enableSubmit();
 		}
 	})
 };
  
 function validateLogin(callback) {
-    var loginToCheck = $('#loginField').val();
+    var loginToCheck = login1.value;
+	console.log(loginToCheck);
 	$.get('checkingLogin.hs', {login: loginToCheck})
 		.success(function() { callback(false) })
 		.error(function() { callback(true) })
 }
 
 function enableSubmit() {
-	if ((email.style.boxShadow == "rgb(0, 255, 0) 0px 0px 4px 0px") && (login.style.boxShadow == "rgb(0, 255, 0) 0px 0px 4px 0px")) {
+	if ((email1.style.boxShadow == "rgb(0, 255, 0) 0px 0px 4px 0px") && (login1.style.boxShadow == "rgb(0, 255, 0) 0px 0px 4px 0px")) {
 		document.getElementsByClassName('submitBtn')[0].style.display = "block";
 	}
 }
