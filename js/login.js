3fc1e4a6381f4e55a1fc7f93168d1c72c661736c
kostinio <shkil-k@mail.ru>
2017-11-28 13:08:44 +0300

    пример строгой типизации

--- js/login.js
+++ js/login.js
@@ -1,17 +1,30 @@
 "use strict";
 
-var email = document.getElementsByClassName('email')[0];
-var pass = document.getElementsByClassName('password')[0];
+// const email = document.getElementsByClassName('email')[0];
+// const pass = document.getElementsByClassName('password')[0];
 
-function check(callback){
-	var emailToCheck = email.value;
-	var passwordToCheck = pass.value;
-	$.get('http://localhost:5002/ajax', {login: emailToCheck, password: passwordToCheck})
-		.success(function() { callback(false) })
-		.error(function() { callback(true) })
+const check = (email, password, callback) => {
+	// const emailToCheck = email.value;
+	// const passwordToCheck = pass.value;
+	$.get('http://localhost:5002/ajax', {login: email, password: password})
+		.success(() => { callback('good') })
+		.error(() => { callback('bad') })
 }
 
-function onSubmit(){
+const onSubmit = () => {
+	const email = document.getElementsByClassName('email')[0].value;
+	const pass  = document.getElementsByClassName('password')[0].value;
+	check(email, pass, (flag) => {
+		if (flag == 'good') {
+			document.cookie = "user_id=".concat(email)
+			window.location.pathname = "/main";
+		} else {
+			window.alert("Неверный логин или пароль!");
+		}
+	});
+	
+};
+	/*
 	check(function(err)
 	{
 		if(err)
@@ -27,5 +40,4 @@ function onSubmit(){
 		return false;
 	}
 	)
-	
-}
+*/
